26b46c2dd66a111faf3581bf389442ebb44c0c5e
HariharaNalamotu <Hari@Hariharas-MacBook-Air.local>
2024-06-22 18:58:18 +0530

    8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,50 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id 'com.android.application'
+    id 'kotlin-android'
+}
+
+android {
+    compileSdkVersion 34
+
+    defaultConfig {
+        applicationId "com.example.test"
+        minSdkVersion 24
+        targetSdkVersion 34
+        versionCode 1
+        versionName "1.0"
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = '1.8'
+    }
+
+    buildFeatures {
+        viewBinding true
+    }
+}
+
+dependencies {
+    implementation 'androidx.core:core-ktx'
+    implementation 'androidx.appcompat:appcompat'
+    implementation 'com.google.android.material:material'
+    implementation 'androidx.constraintlayout:constraintlayout'
+    implementation 'androidx.navigation:navigation-fragment-ktx'
+    implementation 'androidx.navigation:navigation-ui-ktx'
+
+    testImplementation 'junit:junit'
+    androidTestImplementation 'androidx.test.ext:junit'
+    androidTestImplementation 'androidx.test.espresso:espresso-core'
 }
