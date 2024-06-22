1811da196c0b2cb9c7ac3b8c2b17ff441f7e4886
HariharaNalamotu <Hari@Hariharas-MacBook-Air.local>
2024-06-22 18:56:52 +0530

    7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,52 +1,50 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id 'com.android.application'
+    id 'kotlin-android'
 }
 
 android {
-    namespace = "com.example.test"
-    compileSdk = 34
+    compileSdkVersion 34
 
     defaultConfig {
-        applicationId = "com.example.test"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        applicationId "com.example.test"
+        minSdkVersion 24
+        targetSdkVersion 34
+        versionCode 1
+        versionName "1.0"
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = '1.8'
     }
+
     buildFeatures {
-        viewBinding = true
+        viewBinding true
     }
 }
 
 dependencies {
+    implementation 'androidx.core:core-ktx'
+    implementation 'androidx.appcompat:appcompat'
+    implementation 'com.google.android.material:material'
+    implementation 'androidx.constraintlayout:constraintlayout'
+    implementation 'androidx.navigation:navigation-fragment-ktx'
+    implementation 'androidx.navigation:navigation-ui-ktx'
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation 'junit:junit'
+    androidTestImplementation 'androidx.test.ext:junit'
+    androidTestImplementation 'androidx.test.espresso:espresso-core'
 }
